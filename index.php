f1fcb7dac0ab8d2c4ff176b92bccc2da3c07e53a
Ed Steinbauer <steinbauer@fas.harvard.edu>
2017-02-06 20:05:27 -0500

    Made updates to index.php with more info about me, the style.css file for formatting and the quoteRandomizer.php with my favorite quotes

--- index.php
+++ index.php
@@ -9,11 +9,32 @@
 </head>
 <body>
   <h1>Ed Steinbauer</h1>
-  <img src="images/myFamily.jpg" alt="My Family">
-  <h2>About me:</h2>
-  <p>Hello, my name is Ed Steinbauer. This is a picture taken in the summer of 2016 of my family and I while we were on a hike during our vacation in Maine.<p>
-  <p>I have been in the technology industry for over 20 years. I am pursuing a Master of Liberal Arts degree in Information Systems Management at Harvard Extension School.  </p>
-  <h2>One of my favorite quotes: </h2>
-  <p><?=$randomQuote ?></p>
+  <img src="/images/myFamily.jpg" alt="My Family">
+  <div id='aboutMe'>
+    <h2>About me:</h2>
+    <p>
+      Hello, my name is Ed Steinbauer and I am pursuing a Master of Liberal Arts degree in Information Systems Management at Harvard Extension School. This is a picture taken in the summer of 2016 of my family and I while we were on a hike during our vacation in Maine.
+    <p>
+    <p>
+      I've been in the technology industry for the past 23 years, with the last 19 at a small software company called Microsoft. My current role is a service delivery manager for O365, which means I help large enterprise customers move to our cloud-based collaboration services. Exciting times.
+    </p>
+    <p>
+      Even though the dev courses I took while pursuing my Bachelors degree were often my favorite, my first job out of college started my technology path down the infrastructure route and not the development route. While I've always loved development, learning the upcoming infrastructure technologies took a front seat and my dev skills deteriorated over time. Sure, I was able to develop some utilities and scripts to help me and my customers but these opportunities were few and far between.
+    </p>
+    <p>
+      During the years I've developed some personal websites or simple websites for small groups and taught myself by reverse engineering what someone else did as a starting point. I did a lot of bad things over the years and look back at my past work in shame after taking CSCI E-3 and E-12. I have awakened a new found love of development that went dormant all of those years when I was helping customers build out their datacenters, migrating to Exchange Server and so on.
+    </p>
+    <p>
+      Perhaps development work will somehow be a bookmark to my career as it is something I really enjoy. It is great to be able to build something out of nothing that can do really great things.
+    </p>
+    <p>
+      I am looking forward to this class to help me right all the wrongs I've done with my PHP development I did last year!
+    </p>
+    <p>
+      At a personal level, I currently live in the Philadelphia suburbs with my wife and 11 year old son (who I am also teaching web development to), but was born in Ohio to a large farm family.
+    </p>
+    <h2>One of my favorite quotes: </h2>
+    <p><?=$randomQuote ?></p>
+  </div>
 </body>
 </html>
--- quoteRandomizer.php
+++ quoteRandomizer.php
@@ -1,7 +1,13 @@
 <?php
 
 # array of some of my favorite quotes
-$quotes = ['stay cool', 'mondays are fundays', 'be who you are'];
+$quotes = [
+  '"I knew then and I know now that, when it comes to justice, there is no easy way to get it. You can\'t sugarcoat it. You have to take a stand and say, "This is not right."" - Claudette Colvin',
+  '"Ignorance leads to fear, fear leads to hate, and hate leads to violence. This is the equation." - Averroes',
+  '"We must rapidly begin the shift from a "thing-oriented" society to a "person-oriented" society. When machines and computers, profit motives and property rights are considered more important than people, the giant triplets of racism, materialism, and militarism are incapable of being conquered." - Martin Luther King, Jr.',
+  '"Our lives begin to end the day we become silent about things that matter." - Martin Luther King, Jr.',
+  '"You have not failed. You have just found some ways that won\'t work." - unknown (I found this on the underside of a bottle cap on a spike seltzer)'
+];
 
 # use array_rand to return the index of one of the array entries radmomly
 $randomIndex = array_rand($quotes, 1);
